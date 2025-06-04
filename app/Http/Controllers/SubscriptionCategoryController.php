3ccea3eb0d473e8659ceb0db1f07c24a46724288
sas12mita <sasmitapoudel.projectwork@gmail.com>
2025-06-04 11:39:36 +0545

    updated

---
+++ app/Http/Controllers/SubscriptionCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubscriptionCategory;
+use Illuminate\Http\Request;
+
+class SubscriptionCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SubscriptionCategory  $subscriptionCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SubscriptionCategory $subscriptionCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SubscriptionCategory  $subscriptionCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SubscriptionCategory $subscriptionCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SubscriptionCategory  $subscriptionCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SubscriptionCategory $subscriptionCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SubscriptionCategory  $subscriptionCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SubscriptionCategory $subscriptionCategory)
+    {
+        //
+    }
+}
